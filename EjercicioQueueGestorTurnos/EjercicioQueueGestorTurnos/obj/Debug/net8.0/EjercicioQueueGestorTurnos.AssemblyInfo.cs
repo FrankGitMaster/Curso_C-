eacee2b54113819a6d4918d0b82a31a091835084
FrankGitMaster <frank_psn@outllok.com>
2024-12-30 17:00:41 -0500

    Se crea el poryecto para el ejercicio de stack

--- EjercicioQueueGestorTurnos/EjercicioQueueGestorTurnos/obj/Debug/net8.0/EjercicioQueueGestorTurnos.AssemblyInfo.cs
+++ EjercicioQueueGestorTurnos/EjercicioQueueGestorTurnos/obj/Debug/net8.0/EjercicioQueueGestorTurnos.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EjercicioQueueGestorTurnos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3a3e5894c9a7b96801dcc31d29a2f45e5e0a0e4e")]
 [assembly: System.Reflection.AssemblyProductAttribute("EjercicioQueueGestorTurnos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EjercicioQueueGestorTurnos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
