686f88dd25b3b27fc3fa0adbdab45160b8dbd9c4
FrankGitMaster <frank_psn@outllok.com>
2025-01-03 15:45:10 -0500

    Se crea el proyecto

---
+++ Delegados/Delegados/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Predicados
+    {
+
+    class program
+        {
+
+        static void Main(string[] args)
+            {
+
+            List<int> numeros = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            //Predicate<int> esPar = num => num % 2 == 0;
+            Predicate<int> esPar = Matematicas.Pares;
+
+            List<int> listaPares = numeros.FindAll(esPar);
+
+            foreach (int par in listaPares)
+                {
+                Console.WriteLine(par);
+                }
+
+            Console.WriteLine(esPar(101));
+            }
+    }
+
+    class Matematicas
+        {
+
+        public static bool Pares(int num)
+            {
+            return num % 2 == 0;
+            }
+        }
+    }
---
+++ EjercicioPredicados/EjercicioPredicados/Program.cs
@@ -0,0 +1,69 @@
+﻿namespace Predicados
+    {
+
+    class Program
+        {
+
+        public static void Main(string[] args)
+            {
+
+            Producto p1 = new Producto("Pan", 2000);
+            Producto p2 = new Producto("Azucar", 7000);
+            Producto p3 = new Producto("Arina", 3500);
+            Producto p4 = new Producto("Soda", 10000);
+
+        /*  public static bool GetProductoMayor(Producto producto)
+                {
+                if(producto.Precio > 5000)
+                    {
+                    return true;
+                    }
+                else
+                    {
+                    return false;
+                    }
+                }*/
+
+            // Con expresión lambda
+            Predicate<Producto> buscarMayorA = producto => producto.Precio > 5000;
+            List<Producto> listaProductosMayores = Producto.GetListaProductos().FindAll(buscarMayorA);
+
+            Console.WriteLine("Productos con precio mayor a 5000:");
+
+            //foreach (Producto producto in listaProductosMayores) Console.WriteLine(producto.ToString());
+
+            // Con expresión lambda
+            listaProductosMayores.ForEach(producto => Console.WriteLine(producto.ToString()));
+            }
+        }
+
+    class Producto
+        {
+
+        public string Nombre { get; }
+        public decimal Precio { get; }
+        private static List<Producto> _listaProductos = new List<Producto>();
+
+        public Producto(string nombre, decimal precio)
+            {
+            Nombre = nombre;
+            Precio = precio;
+            _listaProductos.Add(this);
+            }
+
+        public static List<Producto> GetListaProductos() => _listaProductos;
+
+        /*        public static void GetTodosLosProductos()
+                    {
+                    foreach (Producto producto in _listaProductos)
+                        {
+                        Console.WriteLine(producto.ToString());
+                        }
+                    }*/
+
+        // Con expresión lambda
+        public static void GetTodosLosProductos() => _listaProductos.ForEach(producto => Console.WriteLine(producto.ToString()));
+
+        public override string ToString() => $"Nombre: {Nombre}\nPrecio: {Precio}\n";
+        }
+    }
