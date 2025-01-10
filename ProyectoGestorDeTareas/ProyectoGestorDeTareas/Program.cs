2009e411311747e9b0fc458514d4f38b0dfc55f1
FrankGitMaster <frank_psn@outllok.com>
2025-01-10 16:00:33 -0500

    Se empieza a crear el menu de la interfaz de usuario

--- ProyectoGestorDeTareas/ProyectoGestorDeTareas/Program.cs
+++ ProyectoGestorDeTareas/ProyectoGestorDeTareas/Program.cs
@@ -5,32 +5,20 @@ namespace ProyectoGestorDeTareas
     class Program
     {
 
+        private static string titulo = "Gestor de tareas";
+        private static string menu = "1. Crear tarea\n2. Editar tarea\n3. Eliminar tarea\n4. Buscar tarea\n5. Ver tareas\n6. Salir";
+
         public static void Main(string[] args)
-        {
+            {
 
-            TareaPendiente tareaPendiente_1 = new TareaPendiente("Tarea pendiente 1", "Descripcion 1", ECategoria.ESTUDIO, EPrioridad.ALTA);
-            TareaPendiente tareaPendiente_2 = new TareaPendiente("Tarea pendiente 2", "Descripcion 2", ECategoria.DEPORTE, EPrioridad.MEDIA);
-            TareaRapida tareaRapida_1 = new TareaRapida("Tarea rapida 1");
-            TareaRapida tareaRapida_2 = new TareaRapida("Tarea rapida 2");
-            TareaUrgente tareaUrgente_1 = new TareaUrgente("Tarea urgente 1", new DateTime(2025, 01, 30, 17, 35, 00));
-            TareaUrgente tareaUrgente_2 = new TareaUrgente("Tarea urgente 2", new DateTime(2025, 08, 07, 02, 00, 00));
-
-            // TAREAS PENDIENTES
-            Console.WriteLine(GestorTareas.FuncionesTarea(GestorTareas.functionAgregarTarea, GestorTareas.GetListaTareasPendientes(), tareaPendiente_1));
-            Console.WriteLine(GestorTareas.FuncionesTarea(GestorTareas.functionAgregarTarea, GestorTareas.GetListaTareasPendientes(), tareaPendiente_2));
-            Console.WriteLine(GestorTareas.VerTareas(GestorTareas.GetListaTareasPendientes()));
-            Console.WriteLine(GestorTareas.FuncionesTarea(GestorTareas.functionEliminarTarea, GestorTareas.GetListaTareasPendientes(), tareaPendiente_1));
-            Console.WriteLine(GestorTareas.VerTareas(GestorTareas.GetListaTareasPendientes()));
-
-            // TAREAS RAPIDAS
-            Console.WriteLine(GestorTareas.FuncionesTarea(GestorTareas.functionAgregarTarea, GestorTareas.GetListaTareasRapidas(), tareaRapida_1));
-            Console.WriteLine(GestorTareas.FuncionesTarea(GestorTareas.functionAgregarTarea, GestorTareas.GetListaTareasRapidas(), tareaRapida_2));
-            Console.WriteLine(GestorTareas.VerTareas(GestorTareas.GetListaTareasRapidas().Values));
-
-            // TAREAS URGENTES
-            Console.WriteLine(GestorTareas.FuncionesTarea(GestorTareas.functionAgregarTarea, GestorTareas.GetListaTareasUrgentes(), tareaUrgente_1));
-            Console.WriteLine(GestorTareas.FuncionesTarea(GestorTareas.functionAgregarTarea, GestorTareas.GetListaTareasUrgentes(), tareaUrgente_2));
-            Console.WriteLine(GestorTareas.VerTareas(GestorTareas.GetListaTareasUrgentes()));
+            Console.WriteLine(titulo.ToUpper() + Environment.NewLine + menu);
+            string opcion = Console.ReadLine();
+            switch (opcion)
+                {
+                case "1":
+                    Console.WriteLine(GestorTareas.FuncionesTarea(GestorTareas.functionAgregarTarea()));
+                    break;
+                }
         }
     }
 }
--- ProyectoGestorDeTareas/ProyectoGestorDeTareas/obj/Debug/net8.0/ProyectoGestorDeTareas.AssemblyInfo.cs
+++ ProyectoGestorDeTareas/ProyectoGestorDeTareas/obj/Debug/net8.0/ProyectoGestorDeTareas.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProyectoGestorDeTareas")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9b336a104b78c1211f10fd7442532d42aaaeef1b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a209d42bda0760eaa1599b40b3bdd691c03066d4")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProyectoGestorDeTareas")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProyectoGestorDeTareas")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
