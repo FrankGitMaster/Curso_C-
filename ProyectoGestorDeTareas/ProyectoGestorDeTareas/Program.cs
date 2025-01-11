fceeea0e9089c3dbd65892dfc461b8bbca1c29d7
FrankGitMaster <frank_psn@outlook.com>
2025-01-11 16:28:32 -0500

    Se crea la funcion para ver las tarea. SE FINALIZA EN PROYECTO PARA CREAR VERSION 2.0

--- ProyectoGestorDeTareas/ProyectoGestorDeTareas/Program.cs
+++ ProyectoGestorDeTareas/ProyectoGestorDeTareas/Program.cs
@@ -10,59 +10,81 @@ namespace ProyectoGestorDeTareas
         private static string txTSeleccioneOpcion = "- Seleccione una opción: ";
         private static string txtTitulo = "Gestor de tareas";
         private static string txtMenu = $"1. Crear tarea\n2. Editar tarea\n3. Eliminar tarea\n4. Buscar tarea\n5. Ver tareas\n6. Salir\n{txTSeleccioneOpcion}";
-        private static string txtMenuTiposTareas = $"\n1. Pendiente\n2. Rapida\n3. Urgente\n{txTSeleccioneOpcion}";
+        private static string txtMenuTiposTareas = $"\n1. Pendiente\n2. Rapida\n3. Urgente\n4. Cancelar\n{txTSeleccioneOpcion}";
 
         public static void Main(string[] args)
         {
-            Console.Write(txtTitulo.ToUpper() + Environment.NewLine + txtMenu);
-            string opcion = Console.ReadLine();
-            switch (opcion)
+            do
             {
-                case "1": // Crear tareas
-                    Console.Write($"De qué tipo es la tarea?\n{txtMenuTiposTareas}");
-                    string opcionTipoTarea = Console.ReadLine();
-                    Console.WriteLine("Ingrese los datos de la tarea");
-                    Console.Write("- Título: ");
-                    string titulo = Console.ReadLine();
-                    switch (opcionTipoTarea)
-                    {
-                        case "1": // Crear tarea pendiente (Título, Descripción, Categoría, Prioridad)
-                            Console.Write("- Descripción: ");
-                            string descripcion = Console.ReadLine();
-                            RecorrerEnum<ECategoria>();
-                            Console.Write(txTSeleccioneOpcion);
-                            ECategoria categoria = (ECategoria)int.Parse(Console.ReadLine());
-                            RecorrerEnum<EPrioridad>();
-                            Console.Write(txTSeleccioneOpcion);
-                            EPrioridad prioridad = (EPrioridad)int.Parse(Console.ReadLine());
-                            Console.WriteLine(GestorTareas.FuncionesTarea(GestorTareas.functionAgregarTarea, GestorTareas.GetListaTareasPendientes(), new TareaPendiente(titulo, descripcion, categoria, prioridad)));
-                            break;
-                        case "2": // Crear tarea rapida (Título)
-                            Console.WriteLine(GestorTareas.FuncionesTarea(GestorTareas.functionAgregarTarea, GestorTareas.GetListaTareasRapidas(), new TareaRapida(titulo)));
-                            break;
-                        case "3": // Crear tarea urgente (Título, FechaHoraVencimiento)
-                            Console.Write("- Fecha y hora de vencimiento (día/mes/año hora:min): ");
-                            string fechaHoraVencimiento = Console.ReadLine();
-                            Console.WriteLine(GestorTareas.FuncionesTarea(GestorTareas.functionAgregarTarea, GestorTareas.GetListaTareasUrgentes(), new TareaUrgente(titulo, DateTime.Parse(fechaHoraVencimiento))));
-                            break;
-                    }
-                    break;
-                case "5":
-                    Console.Write($"De qué tipo es la lista?\n{txtMenuTiposTareas}");
-                    string opcionTipoLista = Console.ReadLine();
-                    /*List<T> tipoLista;*/
-                    switch (opcionTipoLista)
-                    {
-                        case "1":
-                            /*tipoLista = GestorTareas.GetListaTareasPendientes();*/
-                            break;
-                    }
-                    break;
-                case "6":
-                    return;
-            }
+                Console.Write(txtTitulo.ToUpper() + Environment.NewLine + txtMenu);
+                string opcion = Console.ReadLine();
+                switch (opcion)
+                {
+                    case "1": // Crear tareas
+                        do
+                        {
+                            Console.Write($"De qué tipo es la tarea?\n{txtMenuTiposTareas}");
+                            string opcionTipoTarea = Console.ReadLine();
+                            if (opcionTipoTarea.Equals("4")) break;
+                            Console.WriteLine("Ingrese los datos de la tarea");
+                            Console.Write("- Título: ");
+                            string titulo = Console.ReadLine();
+                            switch (opcionTipoTarea)
+                            {
+                                case "1": // Crear tarea pendiente (Título, Descripción, Categoría, Prioridad)
+                                    Console.Write("- Descripción: ");
+                                    string descripcion = Console.ReadLine();
+                                    RecorrerEnum<ECategoria>();
+                                    Console.Write(txTSeleccioneOpcion);
+                                    ECategoria categoria = (ECategoria)int.Parse(Console.ReadLine());
+                                    RecorrerEnum<EPrioridad>();
+                                    Console.Write(txTSeleccioneOpcion);
+                                    EPrioridad prioridad = (EPrioridad)int.Parse(Console.ReadLine());
+                                    Console.WriteLine(GestorTareas.FuncionesTarea(GestorTareas.functionAgregarTarea, GestorTareas.GetListaTareasPendientes(), new TareaPendiente(titulo, descripcion, categoria, prioridad)));
+                                    break;
+                                case "2": // Crear tarea rapida (Título)
+                                    Console.WriteLine(GestorTareas.FuncionesTarea(GestorTareas.functionAgregarTarea, GestorTareas.GetListaTareasRapidas(), new TareaRapida(titulo)));
+                                    break;
+                                case "3": // Crear tarea urgente (Título, FechaHoraVencimiento)
+                                    Console.Write("- Fecha y hora de vencimiento (día/mes/año hora:min): ");
+                                    string fechaHoraVencimiento = Console.ReadLine();
+                                    Console.WriteLine(GestorTareas.FuncionesTarea(GestorTareas.functionAgregarTarea, GestorTareas.GetListaTareasUrgentes(), new TareaUrgente(titulo, DateTime.Parse(fechaHoraVencimiento))));
+                                    break;
+                            }
+                            Console.WriteLine("Quiere crear otra tarea? 1.Si 2.No");
+                            string crearOtraTarea = Console.ReadLine();
+                            if (crearOtraTarea.Equals("1", StringComparison.OrdinalIgnoreCase)) continue;
+                            else break;
+                        } while (true);
+                        continue;
+                    case "5":
+                        Console.Write($"De qué tipo es la lista?\n{txtMenuTiposTareas}");
+                        string opcionTipoLista = Console.ReadLine();
+                        if (opcionTipoLista.Equals("4")) break;
+                        IEnumerable<Tarea> lista;
+                        switch (opcionTipoLista)
+                        {
+                            case "1":
+                                lista = GestorTareas.GetListaTareasPendientes();
+                                Console.WriteLine(GestorTareas.VerTareas(lista));
+                                break;
+                            case "2":
+                                lista = GestorTareas.GetListaTareasRapidas().Values;
+                                Console.WriteLine(GestorTareas.VerTareas(lista));
+                                break;
+                            case "3":
+                                lista = GestorTareas.GetListaTareasUrgentes();
+                                Console.WriteLine(GestorTareas.VerTareas(lista));
+                                break;
+                        }
+                        break;
+                    case "6":
+                        return;
+                }
+            } while (true);
         }
 
+        // Metodo para recorrer un enum e imprimir su valores.
         public static void RecorrerEnum<T>() where T : Enum
         {
             string[] arrayNames = Enum.GetNames(typeof(T));
--- ProyectoGestorDeTareas/ProyectoGestorDeTareas/obj/Debug/net8.0/ProyectoGestorDeTareas.AssemblyInfo.cs
+++ ProyectoGestorDeTareas/ProyectoGestorDeTareas/obj/Debug/net8.0/ProyectoGestorDeTareas.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProyectoGestorDeTareas")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2009e411311747e9b0fc458514d4f38b0dfc55f1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9cb088dd65f4d04dff3c69fc116a8b2cfbbc5d23")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProyectoGestorDeTareas")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProyectoGestorDeTareas")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
