01ee6e6a855b71a5b244cc4a00cd543a6eb5b114
Frank Aguilar <104644363+FrankGitMaster@users.noreply.github.com>
2025-01-13 22:23:41 -0500

    Add files via upload

---
+++ GestorNotas - copia (2)/GestorNotas/ListaTareas.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+
+namespace GestorNotas
+{
+    class ListaTareas : ListaBase
+    {
+
+        private LinkedList<Tarea> _listaTareas = new LinkedList<Tarea>();
+
+        // Crear una nueva nota en la lista y retornar mensaje que informa si la nota ha sido agregada con exito
+        public string CrearTarea(string nombre, string descripcion, int categoria, string fechaHoraVencimiento)
+        {
+            string resultado = "";
+            try
+            {
+                Tarea nuevaTarea = new Tarea(nombre, descripcion, (Tarea.CategoriaTarea)categoria, fechaHoraVencimiento);
+                if (categoria > Enum.GetValues(typeof(Tarea.CategoriaTarea)).Length || categoria <= 0)
+                {
+                    resultado = "(x) Error al crear la tarea: La categoría ingresada no es válida";
+                    Console.ForegroundColor = ConsoleColor.Red;
+                }
+                else
+                {
+                    // Agregar la tarea a la lista
+                    _listaTareas.AddLast(nuevaTarea);
+                    resultado = "(i) Tarea creada";
+                    Console.ForegroundColor = ConsoleColor.Green;
+                }
+            }
+            catch (FormatException ex)
+            {
+                // Aquí capturamos la excepción de formato lanzada en el constructor de Tarea
+                resultado = $"(x) Error al crear la tarea: {ex.Message}";
+                Console.ForegroundColor = ConsoleColor.Red;
+            }
+            catch (Exception ex)
+            {
+                resultado = $"(x) Error inesperado: {ex.Message} {ex.StackTrace}";
+                Console.ForegroundColor = ConsoleColor.Red;
+            }
+            return resultado;
+        }
+
+        public override string RetornarElementosLista()
+        {
+            string tareas = "";
+            _listaTareas.OrderBy(tarea => tarea.FechaHoraCreacion).ToList().ForEach(tarea => tareas += tarea.ToString());
+            Console.ResetColor();
+            return tareas;
+        }
+
+        public override string VerElementosLista()
+        {
+            return $"TAREAS:\n{(_listaTareas.Count > 0 ? RetornarElementosLista() : "No hay tareas")}\n";
+        }
+    }
+}
---
+++ GestorNotas - copia (2)/GestorNotas/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GestorNotas
+{
+
+    class Program
+    {
+
+        private static List<Base> _listaPrincipal = new List<Base>();
+
+        public static void Main(string[] args)
+        {
+
+            do
+            {
+                Console.WriteLine("GESTOR DE NOTAS Y TAREAS\n");
+
+                List<Base> listaPrincipalOrdenada = _listaPrincipal.OrderByDescending(e => e.FechaHoraCreacion).ToList();
+                foreach (var elemento in listaPrincipalOrdenada)
+                {
+                    Console.WriteLine(elemento.ToString());
+                }
+                if (listaPrincipalOrdenada.Count == 0) Console.WriteLine("No hay notas ni tareas\n");
+
+                Console.Write("1. Crear nota\n2. Crear Tarea\n - Seleccione una opción: ");
+                string opcionMenuPrincipal = Console.ReadLine();
+                switch (opcionMenuPrincipal)
+                {
+                    case "1":
+                        Console.Write("CREAR NOTA\n1. Nota rápida\n2. Lista de notas\n - Seleccione una opción: ");
+                        string opcionCrearNota = Console.ReadLine();
+                        switch (opcionCrearNota)
+                        {
+                            case "1":
+                                Console.Write("NOTA RAPIDA\n - Ingrese el título: ");
+                                string titulo = Console.ReadLine();
+                                Console.Write(" - Ingrese la descripción: ");
+                                string descripcion = Console.ReadLine();
+                                NotaRapida nuevaNotaRapida = new NotaRapida(titulo, descripcion);
+                                _listaPrincipal.Add(nuevaNotaRapida);
+                                break;
+                            case "2":
+                                Console.Write("LISTA DE NOTAS\n1. Crear Lista\n2. Crear nota en lista\nSeleccione una opción: ");
+                                string opcionListaNotas = Console.ReadLine();
+                                switch (opcionListaNotas)
+                                {
+                                    case "1":
+                                        Console.Write("LISTA DE NOTAS\n - Ingrese el título de la lista: ");
+                                        string tituloListaNotas = Console.ReadLine();
+                                        ListaNotas nuevaListaNotas = new ListaNotas(tituloListaNotas);
+                                        _listaPrincipal.Add(nuevaListaNotas);
+                                        break;
+                                    case "2":
+                                        List<Base> listasNotasSeleccionada = _listaPrincipal.Where(lista => lista is ListaNotas).ToList();
+                                        if (listasNotasSeleccionada.ToList().Count <= 0)
+                                        {
+                                            Console.ForegroundColor = ConsoleColor.Red;
+                                            Console.WriteLine("(x) No hay lista creadas");
+                                            Console.ResetColor();
+                                            break;
+                                        }
+                                        else
+                                        {
+                                            List<ListaNotas> listaN = listasNotasSeleccionada.OfType<ListaNotas>().ToList();
+                                            for (int i = 0; i < listaN.Count; i++) Console.WriteLine($"{i + 1}. {listaN[i]}");
+                                            do
+                                            {
+                                                Console.Write("LISTA DE NOTAS\n - Seleccione una lista: ");
+                                                if (!int.TryParse(Console.ReadLine(), out int opcionListaNotasSeleccionada) || opcionListaNotasSeleccionada > listaN.Count || opcionListaNotasSeleccionada <= 0)
+                                                {
+                                                    Console.ForegroundColor = ConsoleColor.Red;
+                                                    Console.WriteLine("(x) Error: Seleccione una lista válida");
+                                                    Console.ResetColor();
+                                                    continue;
+                                                }
+                                                else
+                                                {
+                                                    Console.Write(" - Ingrese el título de la nota: ");
+                                                    string tituloNota = Console.ReadLine();
+                                                    listaN[opcionListaNotasSeleccionada - 1].CrearNota(tituloNota);
+                                                    break;
+                                                }
+                                            } while (true);
+                                        }
+                                        break;
+                                }
+                                break;
+                        }
+                        break;
+                }
+                continue;
+                break;
+            } while (true);
+        }
+    }
+}
